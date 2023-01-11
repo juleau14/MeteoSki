2d1bfea0ab6c2916e3989650879f6b7422beb646
Jules <jcamuzet@yahoo.com>
2023-01-11 14:19:54 +0100

    first commit

--- db.js
+++ db.js
@@ -3,83 +3,53 @@ const mysql = require('mysql2');
 const request = require('request');
 
 
+const connectToDb = () => {
 
-const updateData = async () => {
-
-    const dbConnection = mysql.createConnection({   // create connection object to db
-        host: 'localhost',
-        user: 'juleau',
-        password: 'LeCafePop',
-        database: 'skidb',
-    });
-    
-    dbConnection.connect( (err) => {
-        if (err) throw err;
-        console.log("You are connected to DB");
-    });
-    
-    dbConnection.query(`SELECT name FROM stations`, (err, rows, fields) => {    // get all station names from db (stored in rows in array of JSONs)
-
-        let names = rows.map( obj => obj["name"]);      // making array with all names 
-
-        (async () => {
-            for (let i = 0; i < names.length; i++) {        // for each name we request data from api and update db with it
-
-                // we absolutely need to wait for previous request to api
-                // to be finished before launching another one or it is DEATH
-                // so we make a promise 
-
-                let promise = new Promise( (resolve, reject) => {
-
-                    const options = {           // here are the options we gonna use to send req to api 
-                        method: 'GET',
-                        url: `https://ski-resort-forecast.p.rapidapi.com/${names[i]}/snowConditions`,
-                        qs: {units: 'm'},
-                        headers: {
-                        'X-RapidAPI-Key': 'bfecc8036emsh62a26a699b53913p109d58jsn52aea8b3efb7',
-                        'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com',
-                        useQueryString: true
-                        }
-                    };
-
-                    request(options, (err, res, body) => {
-
-                        const snowData = JSON.parse(body);
-
-                        const stationName = snowData.basicInfo.name;
-                        const topSnow = snowData.topSnowDepth;
-                        const botSnow = snowData.botSnowDepth;
-
-                        console.log(stationName, topSnow, botSnow);
-
-                        dbConnection.query(`UPDATE stations SET topsnow='${topSnow}' WHERE name='${names[i]}';`, (err) => {
-                            if (err) throw err;
-                            console.log('Updated topsnow for '+stationName);
-                        });
-                        
-                        dbConnection.query(`UPDATE stations SET botsnow='${botSnow}' WHERE name='${names[i]}';`, (err) => {
-                            if (err) throw err;
-                            console.log('Updated botsnow for '+stationName);
-                        });
-
-
-                        resolve("OK");
-
-                    })
-
-                });
-
-                await promise;
-               
-            }
-
-        })();
-
+    return new Promise((resolve, reject) => {
         
+        const dbConnection = mysql.createConnection({   // create connection object to db
+            host: 'localhost',
+            user: 'juleau',
+            password: 'LeCafePop',
+            database: 'skidb',
+        });
+        
+        dbConnection.connect( (err) => {
+            if (err) throw err;
+            console.log("You are connected to DB");
+
+            resolve(dbConnection);
+        });
+    })
+
+}
+
+
+const getNamesFromDb = (dbConnection) => {
+    
+    return new Promise((resolve, reject) => {
+            
+        dbConnection.query(`SELECT name FROM stations`), (err, rows, fields) => {
+                console.log("J'ai bien récupéré les noms dans la DB, je peux resolve la promesse avec ces derniers !");
+                resolve(rows);
+            }
 
     });
 
 }
 
+
+const updateData = async () => {
+
+    const dbConnection = await connectToDb();
+    
+    const names = await getNamesFromDb(dbConnection);
+
+    console.log(names);
+
+}
+
+
+updateData();
 
 module.exports = updateData;
