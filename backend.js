8c993b1857ddb1aed1c088f77ded1890378abda5
Jules <jcamuzet@yahoo.com>
2023-01-11 15:04:48 +0100

    finishing to remake the db system, now its more scalable and cleaner

--- backend.js
+++ backend.js
@@ -1,13 +1,14 @@
 const request = require('request');
 const mysql2 = require('mysql2');
 const dbConnection = require('./db');
+const { map } = require('jquery');
 
 
 const getNamesFromDb = () => {
 
     return new Promise((resolve, reject) => {
         
-        dbConnection.query(`SELECT * name FROM stations;`, (err, rows, fields) => {
+        dbConnection.query(`SELECT name FROM stations;`, (err, rows, fields) => {
 
             if (err) throw err;
 
@@ -22,11 +23,111 @@ const getNamesFromDb = () => {
 }
 
 
+const requestData = (stationName) => {
+    
+    return new Promise((resolve, reject) => {
+        
+        const options = {
+            method: 'GET',
+            url: `https://ski-resort-forecast.p.rapidapi.com/${stationName}/snowConditions`,
+            qs: {units: 'm'},
+            headers: {
+              'X-RapidAPI-Key': 'bfecc8036emsh62a26a699b53913p109d58jsn52aea8b3efb7',
+              'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com',
+              useQueryString: true
+            }
+        };
+
+        console.log(`Requête envoyée pour les données de ${stationName}`);
+
+        request(options, (err, res, body) => {
+
+            if (err) throw err;
+
+            console.log("Nous avons bien recu les données de la station "+stationName+".");
+
+            resolve(JSON.parse(body));
+
+        });
+
+    })
+
+}
+
+
+const updateTopSnow = (newValue, stationName) => {
+
+    return new Promise((resolve, reject) => {
+        
+        dbConnection.query(`UPDATE stations SET topsnow='${newValue}' WHERE name='${stationName}';`, (err) => {
+
+            if (err) throw err;
+
+            console.log('Topsnow mis à jour pour '+stationName+'.');
+
+            resolve();
+
+        });
+
+    })
+
+}
+
+
+const updateBotSnow = (newValue, stationName) => {
+
+    return new Promise((resolve, reject) => {
+        
+        dbConnection.query(`UPDATE stations SET botsnow='${newValue}' WHERE name='${stationName}';`, (err) => {
+
+            if (err) throw err;
+
+            console.log('Botsnow mis à jour pour '+stationName+'.');
+
+            resolve();
+
+        });
+
+    })
+
+}
+
+
+const updateDb = async (stationData, stationName) => {
+
+    return new Promise((resolve, reject) => {
+        
+        (async () => {
+
+            await updateTopSnow(stationData.topSnowDepth, stationName);
+            await updateBotSnow(stationData.botSnowDepth, stationName);
+
+            resolve(`Les données de ${stationName} ont bien été mises à jours.`);
+
+        })();
+
+    })
+
+}
+
+
 const updateData = async () => {
 
-    const names = await getNamesFromDb();
+    const namesFromDb = await getNamesFromDb();
 
-    console.log(names);
+    const names = namesFromDb.map(el => el.name);   // names = ['name1', 'names2', ...]
+
+    for (let i = 0; i < names.length; i++) {
+
+        var stationName = names[i];
+
+        var stationData = await requestData(names[i]);      // stationData = JSON
+
+        var updateMessage = await updateDb(stationData, stationName);
+
+        console.log(updateMessage, '\n\n');
+
+    }
 
 }
 
