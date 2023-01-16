f02df7751522e34f2bc7ae0279f17868afc513fd
Jules <jcamuzet@yahoo.com>
2023-01-16 23:10:35 +0100

    remaking baack.js

--- backend.js
+++ backend.js
@@ -4,17 +4,43 @@ const dbConnection = require('./db');
 const { map } = require('jquery');
 
 
-const getNamesFromDb = () => {
+const getNamesFromDb =  () => {
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => { 
         
-        dbConnection.query(`SELECT name FROM stations;`, (err, rows, fields) => {
+        dbConnection.query(`SELECT name FROM stations`, (err, result, fields) => {
 
             if (err) throw err;
+            
+            const names = result.map(el => el.name);
+            resolve(names);
 
-            console.log("Noms bien récupérés de la base de données.");
+        });
 
-            resolve(rows);
+    });
+       
+}
+
+const requestForecast = async (stationName) => {                // request du forecast d'UNE station
+    
+    return new Promise((resolve, reject) => {
+
+        const options = {
+            method: 'GET',
+            url: `https://ski-resort-forecast.p.rapidapi.com/${stationName}/forecast`,
+            qs: {units: 'm', el: 'top'},
+            headers: {
+                'X-RapidAPI-Key': 'bfecc8036emsh62a26a699b53913p109d58jsn52aea8b3efb7',
+                'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com',
+                useQueryString: true
+            }
+        };
+
+        request(options, function (error, response, body) {
+
+            if (error) throw new Error(error);
+
+            resolve(body);
 
         });
 
@@ -23,113 +49,37 @@ const getNamesFromDb = () => {
 }
 
 
-const requestData = (stationName) => {
-    
+const requestSnowConditions = async (stationName) => {          // request des snow conditions d'UNE station
+
     return new Promise((resolve, reject) => {
         
         const options = {
             method: 'GET',
             url: `https://ski-resort-forecast.p.rapidapi.com/${stationName}/snowConditions`,
-            qs: {units: 'm'},
+            qs: {units: 'm', el: 'top'},
             headers: {
-              'X-RapidAPI-Key': 'bfecc8036emsh62a26a699b53913p109d58jsn52aea8b3efb7',
-              'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com',
-              useQueryString: true
+                'X-RapidAPI-Key': 'bfecc8036emsh62a26a699b53913p109d58jsn52aea8b3efb7',
+                'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com',
+                useQueryString: true
             }
         };
 
-        console.log(`Requête envoyée pour les données de ${stationName}`);
+        request(options, function (error, response, body) {
 
-        request(options, (err, res, body) => {
+            if (error) throw new Error(error);
 
-            if (err) throw err;
-
-            console.log("Nous avons bien recu les données de la station "+stationName+".");
-
-            resolve(JSON.parse(body));
+            resolve(body);
 
         });
 
-    })
+    });
 
 }
 
 
-const updateTopSnow = (newValue, stationName) => {
+const updateSnowConditions
 
-    return new Promise((resolve, reject) => {
-        
-        dbConnection.query(`UPDATE stations SET topsnow='${newValue}' WHERE name='${stationName}';`, (err) => {
 
-            if (err) throw err;
-
-            console.log('Topsnow mis à jour pour '+stationName+'.');
-
-            resolve();
-
-        });
-
-    })
+const fullUpdateStation = async (stationName) => {              // maj complete d'UNE station (requete ET maj mysql)
 
 }
-
-
-const updateBotSnow = (newValue, stationName) => {
-
-    return new Promise((resolve, reject) => {
-        
-        dbConnection.query(`UPDATE stations SET botsnow='${newValue}' WHERE name='${stationName}';`, (err) => {
-
-            if (err) throw err;
-
-            console.log('Botsnow mis à jour pour '+stationName+'.');
-
-            resolve();
-
-        });
-
-    })
-
-}
-
-
-const updateDb = async (stationData, stationName) => {
-
-    return new Promise((resolve, reject) => {
-        
-        (async () => {
-
-            await updateTopSnow(stationData.topSnowDepth, stationName);
-            await updateBotSnow(stationData.botSnowDepth, stationName);
-
-            resolve(`Les données de ${stationName} ont bien été mises à jours.`);
-
-        })();
-
-    })
-
-}
-
-
-const updateData = async () => {
-
-    const namesFromDb = await getNamesFromDb();
-
-    const names = namesFromDb.map(el => el.name);   // names = ['name1', 'names2', ...]
-
-    for (let i = 0; i < names.length; i++) {
-
-        var stationName = names[i];
-
-        var stationData = await requestData(names[i]);      // stationData = JSON
-
-        var updateMessage = await updateDb(stationData, stationName);
-
-        console.log(updateMessage, '\n\n');
-
-    }
-
-}
-
-
-module.exports = updateData;
