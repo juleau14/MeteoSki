50df0c05cc5eeb1de75ee406be4b4d66710685b8
Jules <jcamuzet@yahoo.com>
2023-01-17 17:27:22 +0100

    making path params, but bug cause browser request for css in param

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dataManagement = require('./dataManagement');
 const schedule = require('node-schedule');
+const bodyParser = require('body-parser');
 
 
 const app = express();
@@ -14,7 +15,11 @@ app.set('view engine', 'hbs');
 
 app.use(express.static('static'));
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.get('*', routes);
+app.post('*', routes);
 
 app.listen(3000, () => {
     console.log("App running on port 3000 !");
--- routes.js
+++ routes.js
@@ -5,18 +5,33 @@ const dataManagement = require('./dataManagement');
 const router = express.Router();
 
 
-router.use('/home', async (req, res) => {
+router.get('/home/:opt/', async (req, res) => {
 
-    const data = await dataManagement.getFromDb();
+    const data = await dataManagement.getAllStationsFromDb();
+    console.log(req.path);
     res.render('home.hbs', data);
 
 });
 
 
-router.use('/details', (req, res) => {
+router.post('/home', (req, res) => {
+
+    res.redirect(`/infos/${req.body.search}`);
+
+});
+
+
+router.get('/infos', (req, res) => {
     res.render('station_info.hbs');
 })
 
+
+router.get('/infos/:stationName', async (req, res) => {
+    const station = req.params.stationName;
+    const data = await dataManagement.makeDataForInfoPage(station);
+    console.log(req.path);
+    res.render('station_info.hbs', data);
+})
 
 
 // any other url redirect to home 
